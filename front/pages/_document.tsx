15b5c73d6b03a5e9ac24d6a72549b36e421f092c
esrafilelahi <essinho1121@gmail.com>
2022-10-06 22:12:28 +0330

    add fonts

--- front/pages/_document.tsx
+++ front/pages/_document.tsx
@@ -14,6 +14,9 @@ class MyDocument extends Document {
       <Html>
         <title>typescript nextjs express auth</title>
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Bad+Script&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
