aa07fd8e0ecd4c97cba5d32ccb0ae805a06f6a19
esrafilelahi <essinho1121@gmail.com>
2022-10-13 19:39:11 +0330

    add pagination

--- front/pages/users/index.tsx
+++ front/pages/users/index.tsx
@@ -7,27 +7,33 @@ type Props = {}
 const Users: NextPageLayout = (props: Props) => {
   return (
     <div className='flex flex-col justify-start items-center w-[30%] h-[80%] rounded-md bg-blue-500 p-3 gap-4'>
-      <h1 className='my-3'>Users List</h1>
+      <h1 className='w-full text-center bg-slate-400 my-2'>Users List</h1>
 
-      <div className=''>
-        <span>1 - <strong>name : </strong>esrafil elahi <strong>email : </strong>esrafil.elahi@gmail.com</span>
-      </div>
-      <div>
-        <span>2 - <strong>name : </strong>esrafil elahi <strong>email : </strong>esrafil.elahi@gmail.com</span>
+      <div className='w-full grow flex flex-col justify-start items-center gap-3 bg-slate-400 overflow-auto py-3'>
+        <div className='w-full px-3 cursor-pointer'>
+          <span>1 : <br /> <strong>name : </strong>esrafil elahi <br /> <strong>email : </strong>esrafil.elahi@gmail.com</span>
+        </div>
+        <div className='w-full px-3 cursor-pointer'>
+          <span>2 : <br /> <strong>name : </strong>esrafil elahi <br /> <strong>email : </strong>esrafil.elahi@gmail.com</span>
+        </div>
+
+        <div className='w-full px-3 cursor-pointer'>
+          <span>3 : <br /> <strong>name : </strong>esrafil elahi <br /> <strong>email : </strong>esrafil.elahi@gmail.com</span>
+        </div>
+
+        <div className='w-full px-3 cursor-pointer'>
+          <span>4 : <br /> <strong>name : </strong>esrafil elahi <br /> <strong>email : </strong>esrafil.elahi@gmail.com</span>
+        </div>
+
+        <div className='w-full px-3 cursor-pointer'>
+          <span>5 : <br /> <strong>name : </strong>esrafil elahi <br /> <strong>email : </strong>esrafil.elahi@gmail.com</span>
+        </div>
       </div>
 
-      <div>
-        <span>3 - <strong>name : </strong>esrafil elahi <strong>email : </strong>esrafil.elahi@gmail.com</span>
+      <div className='w-full flex justify-center gap-4'>
+        <span className='border border-slate-400 rounded-md px-3 py-1 cursor-pointer hover:border-slate-500 hover:text-slate-800'>before</span>
+        <span className='border border-slate-400 rounded-md px-3 py-1 cursor-pointer hover:border-slate-500 hover:text-slate-800'>after</span>
       </div>
-
-      <div>
-        <span>4 - <strong>name : </strong>esrafil elahi <strong>email : </strong>esrafil.elahi@gmail.com</span>
-      </div>
-
-      <div>
-        <span>5 - <strong>name : </strong>esrafil elahi <strong>email : </strong>esrafil.elahi@gmail.com</span>
-      </div>
-
     </div>
   )
 }
