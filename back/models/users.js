aa0313ed90ed7ebe2200f033acd4512286d8ec92
esrafilelahi <essinho1121@gmail.com>
2022-10-14 18:25:20 +0330

    complete user model

--- back/models/users.js
+++ back/models/users.js
@@ -8,5 +8,42 @@ const Users = sequelize.define("Users", {
     allowNull: false,
     defaultValue: DataTypes.UUIDV4
   },
-  
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    DataTypes: DataTypes.STRING,
+    allowNull: false,
+  },
+  confirmPassword: {
+    DataTypes: DataTypes.STRING,
+    allowNull: false,
+  },
+  job: {
+    type: DataTypes.STRING,
+  },
+  birthData: {
+    type: DataTypes.DATEONLY,
+  },
+  age: {
+    type: DataTypes.NUMBER,
+  },
+  gender: {
+    type: DataTypes.STRING
+  },
+  isAdmin: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  }
 })
+
+module.exports = Users;
