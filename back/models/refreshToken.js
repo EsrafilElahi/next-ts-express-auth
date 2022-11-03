904b233b7c2e89e9e5900a5ed27c14d26b1b443b
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-11-03 22:39:41 +0330

    keep coding

---
+++ back/models/refreshToken.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const RefreshTokenSchema = new mongoose.Schema(
+  {
+    userID: {
+      type: Schema.Types.ObjectId,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      expires: 30 * 86400 // 30 days,
+    },
+    expireAt:{
+      type: Date,
+      expires: 30 * 86400 // 30 days,
+    }
+  },
+  // { timestamps: true }, // to build createdAt & updatedAt fields automatically
+  { collections: "refreshToken" }
+);
+
+const UserRefreshTokens = mongoose.model("RefreshToken", RefreshTokenSchema);
+
+module.exports = UserRefreshTokens;
