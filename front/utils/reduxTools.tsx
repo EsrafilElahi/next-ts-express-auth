45cd33a373288c073cf2e4566b59b5b91251cbf1
esrafilelahi <essinho1121@gmail.com>
2023-06-06 01:31:13 +0330

    editing

--- front/utils/reduxTools.tsx
+++ front/utils/reduxTools.tsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import type { TypedUseSelectorHook } from "react-redux";
 import type { AppState, AppDispatch } from "../redux/store";
+
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
